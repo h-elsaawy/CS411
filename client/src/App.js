b950ca79b781107215c868be11d09709aa338c38
pflorid2 <pflorid2@illinois.edu>
2023-11-28 21:06:23 -0600

    hi

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
 import './App.css';
 
 import Home from "./pages/home.jsx"
-import Add from "./pages/add.jsx"
+import Watchlists from "./pages/watchlists.jsx"
 import Videos from "./pages/videos.jsx"
 
 
@@ -16,8 +16,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
-          <Route path="/add" element={<Add/>}></Route>
           <Route path="/videos" element={<Videos/>}></Route>
+          <Route path="/watchlists" element={<Watchlists/>}></Route>
 
         </Routes>
       </BrowserRouter>
---
+++ client/src/pages/watchlists.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Watchlists = () => {
+    return (
+        <div>Watchlists</div>
+    )
+}
+
+export default Watchlists
