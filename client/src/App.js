05e31765a3142c508490e2e36221f433a1436c1c
pflorid2 <pflorid2@illinois.edu>
2023-12-01 09:44:30 -0600

    sdftfafd

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ function App() {
           <Route path="/videos" element={<Videos/>}></Route>
           <Route path="/watchlist" element={<Watchlists/>}></Route>
           <Route path="/login" element={ <Login/>}></Route>
+          sdfghfgh
 
         </Routes>
       </BrowserRouter>
