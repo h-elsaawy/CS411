fb5ca96e819a69434f741805cc32975a61e7a93e
pflorid2 <143139198+pflorid2@users.noreply.github.com>
2023-12-04 15:22:49 -0600

    Pp3 (#6)
    * created page for individual watchlist and links
    
    * no longer having async issues

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Home from "./pages/home.jsx"
 import Watchlists from "./pages/watchlists.jsx"
+import Watchlist from "./pages/watchlist.jsx"
 import Search from "./pages/search.jsx"
 import Dashboard from "./pages/Dashboard.jsx"
 import Login from "./container/Login.js"
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Home/>}></Route>
           <Route path="/search" element={<Search/>}></Route>
           <Route path="/watchlist" element={<Watchlists/>}></Route>
+          <Route path="/watchlist/:id" element={<Watchlist/>}></Route>
           <Route path="/login" element={ <Login/>}></Route>
           <Route path="/register" element={ <Register/>}></Route>
           <Route path="/dashboard" element={ <Dashboard/>}></Route>
