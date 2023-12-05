e8fb5ab51929dbe6f226b96583cea7e8dd3c888e
pflorid2 <pflorid2@illinois.edu>
2023-12-04 18:54:17 -0600

    clean up

--- client/src/pages/watchlists.jsx
+++ client/src/pages/watchlists.jsx
@@ -1,49 +1,44 @@
 import { React, useEffect, useState } from "react";
-import axios from "axios"
-import Navbar from "../container/Navbar.jsx"
-
-
-
+import axios from "axios";
+import Navbar from "../container/Navbar.jsx";
 
 const Watchlists = () => {
     const [watchlists, setWatchlists] = useState([]);
     const username = sessionStorage.getItem('username');
+
     useEffect(() => {
         const fetchAllWatchlists = async () => {
-            try{
-                const url = "http://localhost:8800/getwatchlists/" + username
+            try {
+                const url = "http://localhost:8800/getwatchlists/" + username;
                 const res = await axios.get(url);
-                setWatchlists(res.data);
-                console.log(res.data);
-                sessionStorage.setItem('actual_watchlist', JSON.stringify(res.data))
-                console.log("here");
-                console.log(watchlists);
-                // console.log(watchlists[0]["json_arrayagg(channel_id)"]);
-            } catch (err){
+                const watchlistsData = res.data;
+                setWatchlists(watchlistsData);
+                console.log(watchlistsData);
+                sessionStorage.setItem('actual_watchlist', JSON.stringify(watchlistsData));
+            } catch (err) {
                 console.log(err);
             }
-        }
-        fetchAllWatchlists();
-    });
+        };
 
+        fetchAllWatchlists();
+    }, []); // Run this effect only once on mount
 
     return (
         <div>
             <>
-            {Navbar()}
+                {Navbar()}
             </>
             <h1> Watchlists for {username}: </h1>
             <div className="watchlists">
                 {watchlists.map((watchlist) => (
                     <div key={watchlist.watchlist_id} className="watchlist">
-                        <h2><a href= {`/watchlist/${watchlist.watchlist_id}`}>{watchlist.title}</a> has channels:</h2>
+                        <h2><a href={`/watchlist/${watchlist.watchlist_id}`}>{watchlist.title}</a> has channels:</h2>
                         <h2>{watchlist["json_arrayagg(channel_id)"]}</h2>
-                </div>
+                    </div>
                 ))}
             </div>
-            
         </div>
-    )
-}
+    );
+};
 
-export default Watchlists
+export default Watchlists;
