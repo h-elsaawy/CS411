4fcf1c766a5f8388cb4e6ee26ad5aa3a4c527019
pflorid2 <pflorid2@illinois.edu>
2023-12-05 14:10:35 -0600

    if user doesn't put a new watchlist name, don't attempt to edit

--- client/src/functions/editwatchlistname.jsx
+++ client/src/functions/editwatchlistname.jsx
@@ -3,13 +3,16 @@ import axios from "axios";
 const editWatchlistName = async (watchlist_id, user) => {
     try {
         const new_name = prompt("Enter the new watchlist name: ");
-        const response = await axios.post(`http://localhost:8800/editWatchlist/`, {new_name, watchlist_id, user});
-        if(response.data.success){
-            window.location.reload(true);
+        if(new_name != null){
+            const response = await axios.post(`http://localhost:8800/editWatchlist/`, {new_name, watchlist_id, user});
+            if(response.data.success){
+                window.location.reload(true);
+            }
+            else{
+                alert(response.data.message);
+            }
         }
-        else{
-            alert(response.data.message);
-        }
+        
 
     }
     catch (err) {
