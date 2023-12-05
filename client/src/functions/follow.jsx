cbd01399e1c4324479e13e5f6e224e8882021c11
h-elsaawy <helsaawy@illinois.edu>
2023-12-04 18:43:18 -0600

    still working follow.jsx

--- client/src/functions/follow.jsx
+++ client/src/functions/follow.jsx
@@ -26,20 +26,48 @@ const follow = async (channel_title) => {
         console.log(watchlist_ids, watchlist_titles)
         let watchlist_str = '';
         let num_watchlists = watchlist_ids.length;
-        console.log(num_watchlists)
+
         for (let i=0; i<num_watchlists; i++) {
             console.log(`${watchlist_ids[i]} - ${watchlist_titles[i]} \n`)
             watchlist_str = watchlist_str.concat(`${watchlist_ids[i]} - ${watchlist_titles[i]} \n`)
 
         }
-        console.log(watchlist_str)
-        let watchlist = prompt("Please enter which watchlist to add channel to, or to create new watchlist input unique watchlist title: \n\n" +watchlist_str )
+
+        let selected_watchlist = prompt("Please enter which watchlist to add channel to, or to create new watchlist input unique watchlist title: \n\n" + watchlist_str )
+
+        let comment = prompt("Input comments: \n")
+        // Check to see if the user's selected watchlist is in the user's watchlists?
+        watchlist_titles = watchlist_titles.map(title => title.toLowerCase())
+
+        if (watchlist_ids.includes(parseInt(selected_watchlist)) || watchlist_titles.includes(selected_watchlist.toLowerCase())) {
+            console.log("this worked")
+            if (watchlist_ids.indexOf(parseInt(selected_watchlist)) > -1 ){
+                const request = {
+                    username: sessionStorage.getItem('username'),
+                    watchlist_id: selected_watchlist,
+                    watchlist_title: watchlist_titles[watchlist_ids.indexOf(parseInt(selected_watchlist))],
+                    channel: channel_title,
+                    comments: comment
+                }
+                console.log('watchlist requested was an int: ' + JSON.stringify(request))
+            } else if (watchlist_titles.indexOf(selected_watchlist) > -1) {
+                const request = {
+                    username: sessionStorage.getItem('username'),
+                    watchlist_id: watchlist_ids[watchlist_titles.indexOf(selected_watchlist)],
+                    watchlist_title: selected_watchlist,
+                    channel: channel_title,
+                    comments: comment
+                }
+                console.log('watchlist requested was a string: ' + JSON.stringify(request))
+            }
+
+        }
+
+
 
     } catch (error) {
         console.error("Error fetching watchlists", error);
     }
-
-    console.log("outside the function, " + watchlist_ids)
 
 
     return (
