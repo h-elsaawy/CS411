e439324e8919d06fd05c1cf0cda1efb54e7f103d
Elsaawy <helsaawy@illinois.edu>
2023-12-05 18:17:01 -0600

    saving beofre I start working the sql trigger

--- client/src/functions/editwatchlistname.jsx
+++ client/src/functions/editwatchlistname.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const editWatchlistName = async (watchlist_id, user) => {
     try {
         const new_name = prompt("Enter the new watchlist name: ");
+
         if(new_name != null){
             const response = await axios.post(`http://localhost:8800/editWatchlist/`, {new_name, watchlist_id, user});
             if(response.data.success){
